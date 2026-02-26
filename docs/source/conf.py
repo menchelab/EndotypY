018087ee55651403c6064cf0613031b8240152f1
nerdgum <77642285+meyenberg-mathilde@users.noreply.github.com>
2026-02-26 13:20:08 +0100

    updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,33 +2,37 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../')) # Points to your package root
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon', # For Google/NumPy style docstrings
+    'sphinx.ext.viewcode',
+]
+
 project = 'EndotypY'
 copyright = '2026, Chloé Bûcheron, Iker Núñez-Carpintero, Mathilde Meyenberg, Enes Sakalli'
 author = 'Chloé Bûcheron, Iker Núñez-Carpintero, Mathilde Meyenberg, Enes Sakalli'
-release = '0.2.0'
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
